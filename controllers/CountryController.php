2db8c9fe57a57e59b0eebd1c61b6d5c4311377d1
StaticInsane <benjamin.regato@outlook.com>
2015-02-19 01:34:38 -0600

    Country Created Test

---
+++ controllers/CountryController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+use yii\data\Pagination;
+use app\models\Country;
+
+class CountryController extends Controller
+{
+    public function actionIndex()
+    {
+        $query = Country::find();
+
+        $pagination = new Pagination([
+            'defaultPageSize' => 6,
+            'totalCount' => $query->count(),
+        ]);
+
+        $countries = $query->orderBy('population')
+            ->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+
+        return $this->render('index', [
+            'countries' => $countries,
+            'pagination' => $pagination,
+        ]);
+    }
+}
